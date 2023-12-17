2986cd61b92162e14906f1078a35c6ce1b32f17e
ZhangLianK <zhangliankun0907@foxmail.com>
2023-12-17 10:51:04 +0800

    fix

--- shipping_management/templates/pages/shipping_viewer.py
+++ shipping_management/templates/pages/shipping_viewer.py
@@ -14,18 +14,33 @@ def get_context(context):
             raise frappe.Redirect
 
         context.no_cache = 1
+
         #get driver name
         if scale_item_doc.driver:
             driver_name = frappe.get_value("Driver",scale_item_doc.driver,"full_name")
         context.scale_item = scale_item_doc.name
         context.vehicle = scale_item_doc.vehicle
         context.driver = driver_name
-        context.load_gross_weight = scale_item_doc.load_gross_weight
-        context.load_blank_weight = scale_item_doc.load_blank_weight
-        context.load_net_weight = scale_item_doc.load_net_weight
-        context.offload_gross_weight = scale_item_doc.offload_gross_weight
-        context.offload_blank_weight = scale_item_doc.offload_blank_weight
-        context.offload_net_weight = scale_item_doc.offload_net_weight
+        try:
+            scale_load_log_doc = frappe.get_last_doc("Scale Load Log",filters={"scale_item":scale_item_doc.name})
+            context.load_gross_weight = scale_load_log_doc.load_gross_weight
+            context.load_blank_weight = scale_load_log_doc.load_blank_weight
+            context.load_net_weight = scale_load_log_doc.load_net_weight
+        except Exception as e:
+            context.load_gross_weight = scale_item_doc.load_gross_weight
+            context.load_blank_weight = scale_item_doc.load_blank_weight
+            context.load_net_weight = scale_item_doc.load_net_weight
+
+        try:
+            scale_offload_log_doc = frappe.get_last_doc("Scale Offload Log",filters={"scale_item":scale_item_doc.name})
+            context.offload_gross_weight = scale_offload_log_doc.offload_gross_weight
+            context.offload_blank_weight = scale_offload_log_doc.offload_blank_weight
+            context.offload_net_weight = scale_offload_log_doc.offload_net_weight
+        except Exception as e:
+            context.offload_gross_weight = scale_item_doc.offload_gross_weight
+            context.offload_blank_weight = scale_item_doc.offload_blank_weight
+            context.offload_net_weight = scale_item_doc.offload_net_weight
+
         context.from_addr = scale_item_doc.from_addr
         context.to_addr = scale_item_doc.to_addr
 
--- shipping_management/www/shipping-viewer/index.py
+++ shipping_management/www/shipping-viewer/index.py
@@ -20,12 +20,26 @@ def get_context(context):
         context.scale_item = scale_item_doc.name
         context.vehicle = scale_item_doc.vehicle
         context.driver = driver_name
-        context.load_gross_weight = scale_item_doc.load_gross_weight
-        context.load_blank_weight = scale_item_doc.load_blank_weight
-        context.load_net_weight = scale_item_doc.load_net_weight
-        context.offload_gross_weight = scale_item_doc.offload_gross_weight
-        context.offload_blank_weight = scale_item_doc.offload_blank_weight
-        context.offload_net_weight = scale_item_doc.offload_net_weight
+        try:
+            scale_load_log_doc = frappe.get_last_doc("Scale Load Log",filters={"scale_item":scale_item_doc.name})
+            context.load_gross_weight = scale_load_log_doc.load_gross_weight
+            context.load_blank_weight = scale_load_log_doc.load_blank_weight
+            context.load_net_weight = scale_load_log_doc.load_net_weight
+        except Exception as e:
+            context.load_gross_weight = scale_item_doc.load_gross_weight
+            context.load_blank_weight = scale_item_doc.load_blank_weight
+            context.load_net_weight = scale_item_doc.load_net_weight
+
+        try:
+            scale_offload_log_doc = frappe.get_last_doc("Scale Offload Log",filters={"scale_item":scale_item_doc.name})
+            context.offload_gross_weight = scale_offload_log_doc.offload_gross_weight
+            context.offload_blank_weight = scale_offload_log_doc.offload_blank_weight
+            context.offload_net_weight = scale_offload_log_doc.offload_net_weight
+        except Exception as e:
+            context.offload_gross_weight = scale_item_doc.offload_gross_weight
+            context.offload_blank_weight = scale_item_doc.offload_blank_weight
+            context.offload_net_weight = scale_item_doc.offload_net_weight
+            
         context.from_addr = scale_item_doc.from_addr
         context.to_addr = scale_item_doc.to_addr
         
