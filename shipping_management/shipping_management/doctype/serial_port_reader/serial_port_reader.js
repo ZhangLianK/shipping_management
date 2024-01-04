746391a8d5957b528d3198b907d9868b05d69668
ZhangLianK <zhangliankun0907@foxmail.com>
2024-01-04 11:02:18 +0800

    fix

--- shipping_management/shipping_management/doctype/serial_port_reader/serial_port_reader.js
+++ shipping_management/shipping_management/doctype/serial_port_reader/serial_port_reader.js
@@ -332,6 +332,14 @@ frappe.ui.form.on('Serial Port Reader', {
                     else{
                         frappe.msgprint("保存成功");
                     }
+                    frm.doc.gross_weight = '';
+                    frm.doc.blank_weight = '';
+                    frm.doc.net_weight = '';
+                    frm.doc.scale_item = '';
+                    frm.doc.gross_dt = '';
+                    frm.doc.blank_dt = '';
+                    frm.doc.vehicle = '';
+                    frm.refresh();
                 }
                 else {
                     frappe.msgprint(r.message);
