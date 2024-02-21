0ef7db05796f1ea0cea4d0928a15c4fbb5b09c11
ZhangLianK <zhangliankun0907@foxmail.com>
2024-02-21 09:42:22 +0800

    fix

--- shipping_management/shipping_management/doctype/ship_plan/ship_plan.js
+++ shipping_management/shipping_management/doctype/ship_plan/ship_plan.js
@@ -5,4 +5,8 @@ frappe.ui.form.on('Ship Plan', {
 	// refresh: function(frm) {
 
 	// }
+	from_addr: function(frm) {
+		frm.doc.plan_desc = frm.doc.from_addr
+		frm.refresh_field('plan_desc');
+	},
 });
