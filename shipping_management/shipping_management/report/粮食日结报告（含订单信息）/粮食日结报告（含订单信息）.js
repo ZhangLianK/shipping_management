a4c901fa76d849224a34a6c816c713d0d561f241
ZhangLianK <zhangliankun0907@foxmail.com>
2024-02-27 09:32:08 +0800

    fix

---
+++ shipping_management/shipping_management/report/粮食日结报告（含订单信息）/粮食日结报告（含订单信息）.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Alvin and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["粮食日结报告（含订单信息）"] = {
+	"filters": [
+
+	]
+};
