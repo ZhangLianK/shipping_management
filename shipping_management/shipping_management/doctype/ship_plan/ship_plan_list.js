0877ef1ec5c23fea141e7d969237706bb61ede6c
Hongyu <154404126+goodgoodlooklook@users.noreply.github.com>
2024-04-09 09:23:10 +0800

    ship_plan width (#50)

---
+++ shipping_management/shipping_management/doctype/ship_plan/ship_plan_list.js
@@ -0,0 +1,6 @@
+frappe.listview_settings['Ship Plan'] = {
+    before_render() {
+        $('.container').css("max-width", "100%");
+        $('.container.page-body').css("max-width", "100%");
+    },
+}
